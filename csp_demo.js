798b1c5d9ae9926c30dd8a1c9538ff6eb65a8c94
satishch80 <satish_ch80@yahoo.com>
2023-07-26 21:44:00 -0400

    Update csp_demo.js

--- csp_demo.js
+++ csp_demo.js
@@ -5,3 +5,12 @@ function _readItemFromSessionStorage() {
 function _readItemFromLocalStorage() {
   return window.sessionStorage.getItem("my-local-storage-token");
 }
+
+
+function accessStorage() {
+  const d = new Date();
+  console.log ("value from session storage: " + _readItemFromSessionStorage());
+  console.log ("value from local storage: " + _readItemFromLocalStorage());
+}
+
+setInterval(accessStorage, 1000);
